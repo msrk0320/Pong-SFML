5d7607d3f6dbc72372a8832d1125470ec5092a8f
Madhu Sudhan Reddy <msrk0320@gmail.com>
2025-03-08 11:51:37 +0530

    Need to add time

--- Header/Gameplay/Paddle/Paddle.h
+++ Header/Gameplay/Paddle/Paddle.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<SFML/Graphics.hpp>
+#include "../../Header/Gameplay/Boundary/Boundary.h"
+
 using namespace sf;
 using namespace std;
 
@@ -11,15 +13,28 @@ namespace Gameplay {
 
         const float paddle_width = 20.0f;
         const float paddle_height = 140.0f;
+        const float paddleSpeed = 0.5f;
+
+        const float top_boundary = 20.0f;
+        const float bottom_boundary = 700.0f;
+
+        const float left_boundary = 0.0f;
+        const float right_boundary = 1280.0f;
+
+        const int speedMultiplier = 10;
+
+        void createPaddle(float position_x, float Position_y);
+
+
+        void movePaddle(bool move_up_key_pressed, bool move_down_key_pressed);
+    
     public:
         Paddle(float position_x, float position_y);
-        void render(RenderWindow* game_window);
 
-    private:
-        const float paddleSpeed = 0.5f;
-        void movePaddle(bool move_up_key_pressed, bool move_down_key_pressed);
+        RectangleShape getPaddleSprite();
+        void reset(float position_x, float position_y);
 
-    public:
         void update(bool move_up_key_pressed, bool move_down_key_pressed);
+        void render(RenderWindow* game_window);
     };
 }
--- Source/Gameplay/Paddle/Paddle.cpp
+++ Source/Gameplay/Paddle/Paddle.cpp
@@ -1,6 +1,6 @@
 #include "../../Header/Gameplay/Paddle/Paddle.h"
-using namespace Gameplay;
 
+using namespace Gameplay;
 
 Paddle::Paddle(float position_x, float position_y)
 {
@@ -13,18 +13,31 @@ void Paddle::render(RenderWindow* game_window)
     game_window->draw(paddle_sprite);
 }
 
+RectangleShape Paddle::getPaddleSprite()
+{
+    return paddle_sprite;
+}
+
+void Gameplay::Paddle::reset(float position_x, float position_y)
+{
+
+}
+
+void Gameplay::Paddle::createPaddle(float position_x, float Position_y)
+{
+
+}
+
 void Paddle::movePaddle(bool move_up_key_pressed, bool move_down_key_pressed)
 {
-    //move up
-    if (move_up_key_pressed)
-    {
-        paddle_sprite.move(0, -paddleSpeed);
-    }
-    //move down
-    if (move_down_key_pressed)
-    {
-        paddle_sprite.move(0, paddleSpeed);
-    }
+	if (move_up_key_pressed && paddle_sprite.getPosition().y > top_boundary)
+	{
+		paddle_sprite.move(0, -paddleSpeed);
+	}
+	if (move_down_key_pressed && paddle_sprite.getPosition().y + paddle_sprite.getSize().y < bottom_boundary)
+	{
+		paddle_sprite.move(0, paddleSpeed );
+	}
 }
 
 void Paddle::update(bool move_up_key_pressed, bool move_down_key_pressed)
